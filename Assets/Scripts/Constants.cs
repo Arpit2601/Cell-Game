44649f8e7846fec307ddcf540753f7fbf3ce3acb
SDTheSlayer <dalal.shivang@gmail.com>
2020-10-18 20:03:23 +0530

    basic Movement and enemy

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    [SerializeField]
+    public static float TimeStep =  1f; 
+    public static float distanceDelta =  0.3f; 
+
+     public static float nodeSize =  4.5f; 
+
+}
---
+++ Assets/Scripts/DirectionCellMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections; 
+using System;
+public class DirectionCellMovement : MonoBehaviour
+{
+    private Vector3 targetPosition ;
+    private Vector3 dir ;
+    void Start() {
+        targetPosition = transform.position;  
+        //  GetNextWaypoint();
+    }
+
+    void Update()
+    {
+       	if (Vector3.Distance(transform.position, targetPosition) > Constants.distanceDelta)
+        {
+            Vector3 dir = targetPosition - transform.position;
+            transform.Translate(dir.normalized * Constants.nodeSize * Time.deltaTime / Constants.TimeStep, Space.World);
+        }else{
+           GetNextWaypoint();
+           Debug.Log(transform.forward);
+        }
+        
+    }
+
+
+    void GetNextWaypoint()
+	{
+        targetPosition = transform.position;
+        targetPosition.x = (float)Math.Round(targetPosition.x/Constants.nodeSize);
+        targetPosition.z = (float)Math.Round(targetPosition.z/Constants.nodeSize);
+        targetPosition += transform.forward;
+        targetPosition.x *= Constants.nodeSize;
+        targetPosition.z *= Constants.nodeSize;
+	}
+
+}
